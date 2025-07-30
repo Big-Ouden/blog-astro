0d0bcc3f2074012d93c04975a5f359ef5b24d4c8
BigOuden <simon.belier29@gmail.com>
2025-07-29 23:36:06 -0400

    Some improvements
    I should commit better ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
 
 import tailwindcss from "@tailwindcss/vite";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bigouden.org",
-  integrations: [preact()],
+  integrations: [],
 
   vite: {
     plugins: [tailwindcss()]
