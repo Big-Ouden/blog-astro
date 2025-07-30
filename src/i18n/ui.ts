193bd7cfd45e0e3f5cf1e8c4edcf3c56198391d1
BigOuden <simon.belier29@gmail.com>
2025-07-30 17:43:53 -0400

    add local language routing support, language switch, 404 page

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+    en: 'English',
+    fr: 'Français',
+};
+
+export const showDefaultLang = false;
+
+export const defaultLang = 'fr';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.projects': 'Projects',
+    'nav.tags': 'Tags',
+    'nav.blog': 'Blog',
+  },
+  fr: {
+    'nav.home': 'Accueil',
+    'nav.about': 'À propos',
+    'nav.tags': 'Tags',
+    'nav.blog': 'Blog',
+    'nav.projects': 'Projets',
+  },
+} as const;
