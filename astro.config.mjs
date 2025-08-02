3db92474ab1fc95f3488e94b43f95a55022bee2e
BigOuden <simon.belier29@gmail.com>
2025-08-02 01:09:03 -0400

    add robot.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bigouden.org",
-  integrations: [],
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "fr", // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          fr: "fr-FR",
+          en: "en-US", // The `defaultLocale` value must present in `locales` keys
+        },
+      },
+    }),
+  ],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
