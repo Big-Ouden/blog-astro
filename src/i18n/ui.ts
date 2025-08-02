1b7c5f482434fd66b43ad53e275926734954f5ac
BigOuden <simon.belier29@gmail.com>
2025-08-02 00:35:25 -0400

    add meta desc

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -18,6 +18,8 @@ export const ui = {
     "back": "Back",
     "more": "Load more",
     "resume": "My resume",
+    "meta.desc":
+      "Explore my innovative projects and technical insights as a student specializing in High-Performance Computing and Massive Data at ENSIIE. Dive into my personal blog and professional portfolio dedicated to computing and open source. Find detailed articles and resources for technology enthusiasts.",
   },
   fr: {
     "nav.home": "Accueil",
@@ -29,5 +31,7 @@ export const ui = {
     "back": "Retour",
     "more": "Charger plus",
     "resume": "Mon CV",
+    "meta.desc":
+      "Découvrez mes projets innovants et mes réflexions techniques en tant qu'étudiant en Calculs Intensifs et Données Massives à l'ENSIIE. Explorez mon blog personnel et mon portfolio professionnel dédiés à l'informatique et à l'open source. Trouvez des articles détaillés et des ressources pour les passionnés de technologie.",
   },
 } as const;
